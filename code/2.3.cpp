8893fe68dd53513f3ab30c52ddbf1119316e0e2f
zhangzhen <1099905725@qq.com>
2016-06-20 15:23:33 +0800

    2.3 ok

---
+++ code/2.3.cpp
@@ -0,0 +1,23 @@
+/*EXAMPLE
+ *Input: the node ‘c’ from the linked list a->b->c->d->e
+ *Result: nothing is returned, but the new linked list looks like a->b->d->e
+*/
+
+typedef struct _LinkNode
+{
+    int val;
+    _LinkNode * next;
+}LinkNode, *LinkList;
+
+bool deleteNode(LinkList head, LinkNode& node){
+    if (head == NULL) return false;
+    while (head->next && head->next->val != node.val){
+        head = head->next;
+    }
+    if (head->next == NULL) return false;
+    LinkNode* p = head->next;
+    head->next = p->next;
+    delete p;
+    return true;
+}
+
